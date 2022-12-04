ba02af5e1c8a385e6ff5426b6251478e08a69a7d
andrewrkeen <119800874+andrewrkeen@users.noreply.github.com>
2022-12-04 10:04:44 -0500

    Create aliens1.py

---
+++ aliens1.py
@@ -0,0 +1,62 @@
+import time
+import random
+
+print('Testing, testing....')
+time.sleep(1)
+print('AHhh, YEs, fINaLlY, THe cOnEcCTiOn eS cOMpLEtE!')
+time.sleep(random.uniform(1,5))
+print('hElLO CiTISeNs oF eRTh')
+
+leader = input('wHAts YOur lAdDeR CAlLed? TElL mIe nW!')
+
+print('tAke miE tO', leader)
+
+name = input('aHh YEs, eE fOogot dO ASk dOUr aIm. VUt eEs eEt?') 
+
+print('wElL,', name ,' , pLZd dO AVe yU.')
+
+print('PLZ STAND BY')
+
+print('PLZ STAND BY')
+
+print('PLZ STAND BY')
+
+print('PLZ STAND BY')
+
+print('PLZ STAND BY')
+
+print('loading....loading....loading.....')
+
+print('Later........................................................................................................')
+
+print('WARNING! INCOMING AD!')
+
+print('Hi Guys! Its Jerry The Dinosaur Here, And Today Im ADdressing The Fact That DRAGONS ARE BAD. So We Are Making A Servant System That Involves Dragons! Rent One Today! (I personally HATE these ads. Vive les dragons!(french).)')  
+
+print('now, back to the story......')
+
+how_question = 'sAY,'+ name + ' HOw mUCh lONgER?'
+
+how = input(how_question)
+
+print(how,'!!!!!! tHaTs aH loO0ng tIMe!!!!!!!')
+
+print('"lets skip ahead."')
+
+print('Later.........')
+
+print('wEre ErE!')
+
+print('wEVe REacHEd tHE pLAcE F', leader ,'!')
+
+print('nOW sHAlL vE Go iN?')
+
+print('ElLO,', leader ,'hONeRD dO mEeT U.')
+      
+print(leader , ': Well, well, well, its the troblesome AlienHead and...', name ,'?......Is that you?')
+
+print('You: ........Wyvrn? What?!')
+
+print('                      TO BE CONTINUED                 ')
+      
+
